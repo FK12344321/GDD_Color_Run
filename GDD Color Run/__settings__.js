be7847cadd316cf6376f5d64cf50de10abe5611f
FedorK <fk0606@yandex.ru>
2021-06-16 20:12:18 +0300

    fight_mechanic

---
+++ GDD Color Run/__settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1175865.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 49334134, 49378586, 49432576, 49553575, 49632999, 49759285, 49839988, 49933429 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/49424817/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/49424818/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/49424816/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/49775440/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/49775441/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/49775439/1/ammo.js', 'preload' : true},
+];
